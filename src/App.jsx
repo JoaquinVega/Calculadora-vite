d223a88df5f4d437e64ebd8064200d1b3a5cd024
Joaquin <j.vega49@alumnos.santotomas.cl>
2023-09-01 15:39:48 -0400

    Hola

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import {useState} from 'react';
+
+function App() {
+
+const [input, setInput] = useState('');
+const handleOnClearClick = () => input !== "" ? setInput(""): null;
+
+const agregarInput = val => {
+  setInput(input + val);
+};
+  return (
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo fcc'/>
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'> 
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'> 
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'> 
+          <Boton manejarClic={agregarInput}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={handleOnClearClick}>
+            Clear
+          </BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/componentes/Pantalla.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Stylesheet/Pantalla.css';
+
+const Pantalla = ({ input }) => (
+    <div className='input'>
+        hola
+    </div>
+);
+
+export default Pantalla;
